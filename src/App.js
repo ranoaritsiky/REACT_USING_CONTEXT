4783ea9db7b039c35a3314620fb5217c6645b95a
Tsiky <tsiky@test.com>
2022-07-06 18:51:14 +0300

    imp add component into context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import TodoList from './Components/TodoList';
+import React from "react";
+import TodoList from "./Components/TodoList";
+import Navbar from './Components/Navbar'
 
-import ThemeContextProvider from './Contexts/ThemeContext';
+import ThemeContextProvider from './Contexts/ThemeContext'
+
 
 function App() {
   return (
     <div className="App">
-    <ThemeContextProvider>
-      <TodoList/>
+      <ThemeContextProvider>
 
-    </ThemeContextProvider>
+      <TodoList/>
+      </ThemeContextProvider>
+      <Navbar/>
     </div>
   );
 }
