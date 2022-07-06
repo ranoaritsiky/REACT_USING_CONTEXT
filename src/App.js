40981540b4575fc416d12b19f6d4c306d82d85c5
Tsiky <tsiky@test.com>
2022-07-06 17:30:11 +0300

    add context in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import TodoList from './Components/TodoList';
 
+import ThemeContextProvider from './Contexts/ThemeContext';
+
 function App() {
   return (
     <div className="App">
+    <ThemeContextProvider>
       <TodoList/>
+
+    </ThemeContextProvider>
     </div>
   );
 }
